ff35a76abb33883cc66b38c00c8a6e53825fb234
Jas <ragnarock.josef@gmail.com>
2021-11-23 16:27:09 -0500

    added query to resolver

---
+++ server/schema/resolvers.js
@@ -0,0 +1,21 @@
+import { AuthenticationError } from "apollo-server-express";
+import { User } from "../models"
+import { signToken } from "../utils/auth"
+
+export const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (!context.user) {
+                throw new AuthenticationError("Please Login or Signup");
+            }
+            const userData = await User.findOne({ _id: context.user._id })
+
+            return userData;
+        },
+    },
+
+    Mutation: {
+
+    }
+
+}
