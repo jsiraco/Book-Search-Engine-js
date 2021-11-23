ecb802b874cdc1fe8fcca44b3de8fc64c942783a
Jas <ragnarock.josef@gmail.com>
2021-11-23 16:53:43 -0500

    addUser and Login were added to mutations

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -15,11 +15,27 @@ export const resolvers = {
     },
 
     Mutation: {
+        // Creates user
         addUser: async (parent, args) => {
-
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, User };
         },
         login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            // returns error is user isn't found
+            if (!user) {
+                throw new AuthenticationError("Incorrect Login info");
+            }
 
+            const correctPw = await user.isCorrectPassword(password);
+            // returns error if password is incorrect
+            if (!correctPw) {
+                throw new AuthenticationError("Incorrect Login info");
+            }
+            // if user and password are both correct they are given a web token
+            const token = signToken(user);
+            return { token, user };
         },
         saveBook: async (parent, { bookData }, context) => {
 
