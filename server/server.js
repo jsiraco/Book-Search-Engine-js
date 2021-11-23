84b4b40f461283c9d2223cf890647422ed42d469
Jas <ragnarock.josef@gmail.com>
2021-11-23 18:53:27 -0500

    changed server.js

--- server/server.js
+++ server/server.js
@@ -6,15 +6,23 @@ const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+const PORT = process.env.PORT || 3001;
 
-server.applyMiddleware({ app });
+let server = null;
+
+async function startServer() {
+  server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -22,11 +30,12 @@ app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 db.once('open', () => {
   app.listen(PORT, () => {
